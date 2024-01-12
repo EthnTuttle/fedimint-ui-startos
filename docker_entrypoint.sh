c3ebd59f12bf37c94d2cdceaf6dc214a344ae132
Ethan Tuttle <tuttle.ethan@protonmail.com>
2024-01-12 08:56:27 -0500

    docker entrypoint first pass

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -1,3 +1,12 @@
-#!/bin/sh
+#!/bin/bash
 
-exec tini hello-world
+set -e
+
+echo "Setting environment variables..."
+
+export PORT=3000
+export FEDIMINT_API=$(yq e '.fedimintd-config-api' /data/start9/config.yaml)
+export REACT_APP_FM_GATEWAY_API=${FEDIMINT_API}
+
+1>&2 echo 'Starting Guardian UI...'
+node scripts/replace-react-env.js build && serve -s build
