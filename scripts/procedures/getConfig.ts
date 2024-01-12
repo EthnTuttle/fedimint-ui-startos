5ea73d39a841d546ffb0a3e6809253468308aaf8
Ethan Tuttle <tuttle.ethan@protonmail.com>
2024-01-12 14:16:33 -0500

    getConfig

--- scripts/procedures/getConfig.ts
+++ scripts/procedures/getConfig.ts
@@ -1,5 +1,15 @@
 // To utilize the default config system built, this file is required. It defines the *structure* of the configuration file. These structured options display as changeable UI elements within the "Config" section of the service details page in the StartOS UI.
-
 import { compat, types as T } from "../deps.ts";
 
-export const getConfig: T.ExpectedExports.getConfig = compat.getConfig({});
+export const getConfig: T.ExpectedExports.getConfig = compat.getConfig({
+  "fedimintd-config-api": {
+    type: "pointer",
+    name: "Fedimintd Config API",
+    description: "The endpoint to use when connecting to the fedimint server to be managed.",
+    subtype: "package",
+    "package-id": "fedimintd",
+    target: "config",
+    multi: false,
+    selector: "$.api.config",
+  },
+});
